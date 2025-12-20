0c18be3ca6058dffc847880fdda6f2900550843a
Garrett <stmichaelboy@icloud.com>
2025-12-20 13:09:12 -0500

    Update buttontoled.ino
    This makes an led turn on when you push a button

--- buttontoled.ino
+++ buttontoled.ino
@@ -1,17 +1,19 @@
+//this makes an led turn on when you push the button
+
 const int buttonPin = 4;
 const int ledPin = 8;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);  // use internal pull-up
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
   int state = digitalRead(buttonPin);
-
-  if (state == LOW) {        // pressed (connected to GND)
+  
+  if (state == LOW) {
     digitalWrite(ledPin, HIGH);
-  } else {                   // not pressed
+  } else {
     digitalWrite(ledPin, LOW);
   }
 }
