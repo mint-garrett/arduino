bdbb56652cbb9437e5da1905757d2cb07138793e
Garrett <stmichaelboy@icloud.com>
2025-12-20 13:06:58 -0500

    Update first.ino

--- first.ino
+++ first.ino
@@ -1,17 +1,20 @@
+//this batch of code makes two leds blink at different time intervals
+
 
 void setup(){
-  // put your setup code here, to run once:
+  
 int ledPin=13;
 pinMode(ledPin,OUTPUT);
+  
 int ledPin2 = 12;
 pinMode(ledPin2, OUTPUT);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 int ledPin=13;
 int ledPin2=12;
+
 digitalWrite(ledPin, HIGH);
 delay(1000);
 digitalWrite(ledPin, LOW);
