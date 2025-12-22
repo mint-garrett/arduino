4f1c3319563d91e9a2213d74e96f03035ceb9a56
Garrett <stmichaelboy@icloud.com>
2025-12-22 10:54:17 -0500

    Add files via upload

---
+++ trafficlight.ino
@@ -0,0 +1,23 @@
+void setup() {
+  int red = 13, yellow = 12, green = 11;
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  int red = 13, yellow = 12, green = 11;
+  //red turning on and off
+  digitalWrite(red, HIGH);
+  delay(5000);
+  digitalWrite(red, LOW);
+  //yellow turning on and off
+  digitalWrite(yellow, HIGH);
+  delay(5000);
+  digitalWrite(yellow, LOW);
+  //green turning on/off
+  digitalWrite(green,HIGH);
+  delay(5000);
+  digitalWrite(green,LOW);
+
+}
