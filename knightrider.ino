76af1c2c4d5543f180475b5fdda176ab05b04e34
Garrett <stmichaelboy@icloud.com>
2026-02-18 13:09:37 -0500

    Add files via upload

---
+++ knightrider.ino
@@ -0,0 +1,23 @@
+//knight rider
+int leds[] = {2,3,4,5,6,7};
+
+void setup() {
+    for(int i=0; i<6; i++) {
+    pinMode(leds[i], OUTPUT);
+    }
+}
+
+void loop() {
+  ///leds transitioning forward
+  for (int i=0; i<6; i++){
+    digitalWrite(leds[i], HIGH);
+    delay(150);
+    digitalWrite(leds[i],LOW);
+    }
+  //leds transitioning backward
+  for (int i=5; i>=0; i--){
+     digitalWrite(leds[1],HIGH);
+    delay(150);
+    digitalWrite(leds[i],LOW);
+    }  
+}
