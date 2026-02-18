7cefc5a2c0461fefc2528c77082e5f9308a8a435
Garrett <stmichaelboy@icloud.com>
2026-02-18 13:10:40 -0500

    Update knightrider.ino

--- knightrider.ino
+++ knightrider.ino
@@ -2,20 +2,23 @@
 int leds[] = {2,3,4,5,6,7};
 
 void setup() {
-    for(int i=0; i<6; i++) {
-    pinMode(leds[i], OUTPUT);
+    for(int i=0; i<6; i++) 
+    {
+        pinMode(leds[i], OUTPUT);
     }
 }
 
 void loop() {
   ///leds transitioning forward
-  for (int i=0; i<6; i++){
-    digitalWrite(leds[i], HIGH);
-    delay(150);
-    digitalWrite(leds[i],LOW);
+  for (int i=0; i<6; i++)
+    {
+        digitalWrite(leds[i], HIGH);
+        delay(150);
+        digitalWrite(leds[i],LOW);
     }
   //leds transitioning backward
-  for (int i=5; i>=0; i--){
+  for (int i=5; i>=0; i--)
+  {
      digitalWrite(leds[1],HIGH);
     delay(150);
     digitalWrite(leds[i],LOW);
